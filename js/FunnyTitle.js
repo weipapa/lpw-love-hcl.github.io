ebce84165510dae16293860ba8af79ecde723b06
weipapa <pwlwell@163.com>
2020-06-18 19:59:34 +0800

    Site updated: 2020-06-18 19:59:33

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,12 +3,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/w.ico");
+        $('[rel="icon"]').attr('href', "https://imgkr.cn-bj.ufileos.com/148d5469-390b-4bc2-902c-6d66833fc36b.ico");
         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/w.ico");
+        $('[rel="icon"]').attr('href', "https://imgkr.cn-bj.ufileos.com/148d5469-390b-4bc2-902c-6d66833fc36b.ico");
         document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
